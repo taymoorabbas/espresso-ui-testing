01082f1e83a91bb4a0f7fb3157725970ba09beaf
taymoorabbas <www.taymoor@hotmail.com>
2021-01-14 00:57:08 +0500

    installed kotlin plugin and dependencies and migrated java files to kotlin

---
+++ app/src/main/java/com/taytech/uitesting/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.taytech.uitesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.taytech.uitesting.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
